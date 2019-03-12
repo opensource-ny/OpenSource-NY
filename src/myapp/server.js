7cd2f6cda3368a9ecd5043749085da11bceade7d
gutierrezjdr <gutierrezjdr@gmail.com>
2019-03-12 13:37:42 -0400

    Changed port number to 3000 according to instructions and added login and password text.

--- src/myapp/server.js
+++ src/myapp/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 app.listen( port, () => console.log( `Example app listening on port ${port}!` ) );
 

8544e311e26adf1671c9a1f2efb80ed190bfa1a5
yizongk <yizongk@gmail.com>
2019-03-12 16:47:27 -0400

    broke server down into more modular parts

---
+++ src/myapp/response.js
@@ -0,0 +1,5 @@
+/* Will contain all the response that the server will send out base on requests */
+
+module.exports = (req, res) => {
+    res.send('Hello World!!');
+}
--- src/myapp/server.js
+++ src/myapp/server.js
@@ -1,12 +1,14 @@
+/* Our front line of communication, clients will access this page first for any requests */
+
 const express = require('express');
 const app = express();
 const port = 5000;
+const response = require('./response.js');
+const handleListen = require('./handleListen.js');
 
-app.listen( port, () => console.log( `Example app listening on port ${port}!` ) );
+app.listen( port, handleListen(console.log, port) );
 
-app.get( '/', (req, res) => {
-    res.send('Hello World!')
-} );
+app.get('/', response);
 
 app.get( '/express_backend', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });

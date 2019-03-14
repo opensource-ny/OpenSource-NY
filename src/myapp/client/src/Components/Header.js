72a4cff5786a798d04b565a4aa00c8900850caae
mxmsunny <xiangminmo6121@gmail.com>
2019-03-13 20:08:35 -0400

    added header and some styling

---
+++ src/myapp/client/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+
+function Header(){
+    return(
+        <div className="header">
+            <p className="title">OpenSource NY App</p>
+        </div>
+    )
+}
+
+export default Header

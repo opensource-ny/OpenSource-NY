7d2c73312296b7f27f7015d9c390cc8e142c6673
Chocolate-Spaghet <Andrey.Shtukenberg32@myhunter.cuny.edu>
2019-03-26 09:57:40 -0400

    Folder Rearrangements

--- src/myapp/client/src/Components/PullRequest.js
+++ src/myapp/client/src/Components/PullRequest.js
@@ -8,13 +8,31 @@ class PullRequest extends Component{
             PRnumber: this.props.content.id,
             PRtitle: this.props.content.title,
             PRlink: this.props.content.url,
-            PRstatus: "",
+            PRstatus: this.props.content.status,
+            PRmerge: this.props.content.merged_at
         }
     }
 
     render(){
+        let merge_status
+        if(this.state.PRstatus === "open"){
+            merge_status = <div>
+                Open
+            </div>
+        } else {
+            if(this.state.PRmerge === null){
+                merge_status = <div className="rejected">
+                    Rejected
+                </div>
+            } else {
+                merge_status = <div className="merged">
+                    Merged
+                </div>
+            }
+        }
         return(
             <div className="pullRequest">
+                {merge_status}
                 <div className="prNumber">
                     <a href={this.state.PRlink}>{this.state.PRnumber}</a>
                 </div>

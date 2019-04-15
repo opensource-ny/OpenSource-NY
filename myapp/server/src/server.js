681e1c4d7ce38cb2ebb5ff1f18a924a367869daf
Undid-Iridium <francis.x.irizarry@gmail.com>
2019-04-15 12:36:14 -0400

    There is utilties in node_modules required

--- myapp/server/src/server.js
+++ myapp/server/src/server.js
@@ -1,9 +1,9 @@
 /* Our front line of communication, clients will access this page first for any requests */
 
-const express        = require('express');
+const express = require('express');
 var cors = require('cors')
-const MongoClient    = require('mongodb').MongoClient;//Used to create database for information to go to, online resource.
-const db             = require('config/db');//Used to link to the config of DataBase
+const MongoClient  = require('mongodb').MongoClient;//Used to create database for information to go to, online resource.
+const db = require('config/db');//Used to link to the config of DataBase
 //var database_router = require.resolve('database_route.js');
 const bodyParser = require('body-parser');
 var app = express();
